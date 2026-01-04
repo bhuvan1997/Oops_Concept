0804d94c74a8e974b9e9f49cb69f01f113fe7880
Bhuvan <bhuvangupta8197@gmail.com>
2026-01-04 11:01:52 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Human {
+
+    public $name;
+    public $age;
+
+    public function talk(){
+        echo "Human can talk";
+    }
+
+    public function human_description(){
+        echo "Human name is ".$this->name." and he is ".$this->age." Years old.";
+    }
+
+}
+
+$obj = new Human();
+$obj->name = "Bhuvan Gupta";
+$obj->age = 28;
+$obj->human_description();
